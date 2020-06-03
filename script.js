d846f0f8ef0852412fcc933968b50994f1de29f6
Nader Frigui <nadfri@gmail.com>
2020-06-04 01:22:46 +0200

    modif setData 'text' pour mobile touch

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ window.onload = ()=>
 //     //Applique à un effet à la div qui a subit le drag d'origine
 //     div.ondragend   = ()  => div.classList.remove("dragged"); //supprime la classe dragged
 
-//     //Permet à la div d'etre une zone de drop (refusé par defaut)
+//     //Permet à la div d'etre une zone de drop (refusée par defaut)
 //     div.ondragover  = (e) => e.preventDefault();
 
-//     //Copie la div dragged à la zone du drop et applice un effet CSS dessus
+//     //Copie la div dragged à la zone du drop et applique un effet CSS dessus
 //     div.ondrop      = (e) => 
 //     {
 //         [dragged.innerHTML, div.innerHTML] = [div.innerHTML,dragged.innerHTML]; //interverti les 2 divs
@@ -43,34 +43,40 @@ window.onload = ()=>
 
 /**Version avec dataTransfert.setData() */
 
-const divs = document.querySelectorAll(".draggable");
-let dragged;
+const divs = document.querySelectorAll(".draggable"); //selection de toutes les divs avec la classe draggable
+let dragged; //servira à stocker la div dragged
 for (let div of divs)
 {
-  div.ondragstart     = (e) =>
+  div.ondragstart = (e) =>
   { 
-    dragged = div; //save the element before change 
-    div.classList.toggle("dragged");
-    e.dataTransfer.setData('text/html', div.innerHTML);
+    dragged = div;                                   //copie de la div qui sera dragged
+    div.classList.add("dragged");                   //ajout de la classe dragged
+    e.dataTransfer.setData('text', dragged.innerHTML); //option du drag permettant de sauvegarder le contenu du dragged
   };
 
+  //Applique un effet CSS à l'entrée d'une zone de drop
   div.ondragenter = ()  =>
   {
-    if (!div.classList.contains("dragged")) 
-         div.classList.add('dragEffects'); 
-    div.classList.remove('shake');       
+    if (!div.classList.contains("dragged")) //si ce n'est pas l'element dragged
+         div.classList.add('dragEffects'); //applique la classe dragEffects
+         div.classList.remove('shake');   //Supprime la classe shake si présente
   };
+  //Applique un effet CSS à l'a sortie d'une zone de drop
+  div.ondragleave = ()  => div.classList.remove('dragEffects'); //supprime la classe dragEffects
 
-  div.ondragleave = ()  => div.classList.remove('dragEffects');
-  div.ondragend   = ()  => div.classList.remove("dragged"); 
+  //Applique à un effet à la div qui a subit le drag d'origine
+  div.ondragend   = ()  => div.classList.remove("dragged");  //supprime la classe dragged
+
+  //Permet à la div d'etre une zone de drop (interdit par defaut)
   div.ondragover   = (e) => e.preventDefault();
 
+  //Copie la div dragged à la zone du drop et applique un effet CSS dessus
   div.ondrop = (e) =>
   {
-    dragged.innerHTML = div.innerHTML;
-    div.innerHTML     = e.dataTransfer.getData('text/html');
-    div.classList.remove('dragEffects');
-    div.classList.add("shake"); 
+    dragged.innerHTML = div.innerHTML;                   //le dragged prend la valeur du drop
+    div.innerHTML     = e.dataTransfer.getData('text'); //le drop prend la valeur du dragged
+    div.classList.remove('dragEffects');               //supprime la classe dragEffects
+    div.classList.add("shake");                       //ajout de l'effet shake sur le drop
   };
 
 };
