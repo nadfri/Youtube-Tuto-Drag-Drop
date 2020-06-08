f80ca9f89e994eb9f35503be01b8ac7514563394
Nader Frigui <nadfri@gmail.com>
2020-06-08 17:49:57 +0200

    modif css dragEffectes par dropHover

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ for (let div of divs)
     div.ondragenter = ()  =>
     {
       if (!div.classList.contains("dragged")) //si ce n'est pas l'element dragged
-          div.classList.add('dragEffects'); //applique la classe dragEffects
+          div.classList.add('dropHover'); //applique la classe dropHover
       div.classList.remove('shake');        //Supprime la classe shake si présente
     };
     //Applique un effet CSS à l'a sortie d'une zone de drop
-    div.ondragleave = ()  => div.classList.remove('dragEffects'); //supprime la classe dragEffects
+    div.ondragleave = ()  => div.classList.remove('dropHover'); //supprime la classe dropHover
 
     //Applique à un effet à la div qui a subit le drag d'origine
     div.ondragend   = ()  => div.classList.remove("dragged");  //supprime la classe dragged
@@ -35,7 +35,7 @@ for (let div of divs)
     {
       dragged.innerHTML = div.innerHTML;                   //le dragged prend la valeur du drop
       div.innerHTML     = e.dataTransfer.getData('text/plain'); //le drop prend la valeur du dragged
-      div.classList.remove('dragEffects');               //supprime la classe dragEffects
+      div.classList.remove('dropHover');               //supprime la classe dropHover
       div.classList.add("shake");                       //ajout de l'effet shake sur le drop
     };
 
@@ -58,12 +58,12 @@ for (let div of divs)
 //     div.ondragenter = ()  =>
 //     {
 //         if (!div.classList.contains("dragged")) //si ce n'est pas l'element dragged
-//              div.classList.add('dragEffects'); //applique la classe dragEffects
+//              div.classList.add('dropHover'); //applique la classe dropHover
 //         div.classList.remove('shake');        //Supprime la classe shake si présente
 //     };
 
 //     //Applique un effet CSS à l'a sortie d'une zone de drop
-//     div.ondragleave = ()  => div.classList.remove('dragEffects'); //supprime la classe dragEffects
+//     div.ondragleave = ()  => div.classList.remove('dropHover'); //supprime la classe dropHover
 
 //     //Applique à un effet à la div qui a subit le drag d'origine
 //     div.ondragend   = ()  => div.classList.remove("dragged"); //supprime la classe dragged
@@ -76,7 +76,7 @@ for (let div of divs)
 //     {
 //         [dragged.innerHTML, div.innerHTML] = [div.innerHTML,dragged.innerHTML]; //interverti les 2 divs
 //         //Ex: a=1; b=2;  [a,b]=[b,a]; // maintenant a=2 et b=1
-//         div.classList.remove('dragEffects'); //supprime la classe dragEffects
+//         div.classList.remove('dropHover'); //supprime la classe dropHover
 //         div.classList.add("shake");         //ajout de la classe shake
 //     }
 // }
